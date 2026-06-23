d8757dab9452fb69c04cbcbe8bc01ea18e83875c
elandy <elandy@gmail.com>
2026-06-23 17:33:31 -0300

    Update frontend API_BASE

---
+++ frontend/config.js
@@ -0,0 +1,3 @@
+window.APP_CONFIG = {
+  API_BASE: "https://squaredle-es.fastapicloud.dev/"
+};
--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:8000";
+const API_BASE = window.APP_CONFIG.API_BASE;
 
 export async function getTodayPuzzle() {
     const res = await fetch(`${API_BASE}/puzzle/today`);
