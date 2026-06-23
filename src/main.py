a6d866f9210b2cbb6dccb4a4be7f03b197dab384
elandy <elandy@gmail.com>
2026-06-23 17:44:57 -0300

    Add cors origin.

--- src/main.py
+++ src/main.py
@@ -13,7 +13,9 @@ app = FastAPI(title="Squaredle ES API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # tighten later
+    allow_origins=[
+        "https://squaredle-es.elandy.workers.dev/"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
